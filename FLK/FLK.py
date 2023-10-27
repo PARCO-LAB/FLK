9a58bb1ef6cf2096dff9284a31f869cc34ae3062
Stefano Aldegheri <xaldyz.mail@gmail.com>
2023-10-27 17:08:03 +0200

    Add configurability for bones and EMA filtering

--- FLK/FLK.py
+++ FLK/FLK.py
@@ -1,44 +1,67 @@
+from typing import Optional, List, Any
+import numpy.typing as npt
+
 from .AKF import AKF
 from .BCA import BCA
 from .LPF import EMA
 
-class FLK:
-    
-    def __init__(self,fs,skeleton,keypoints,model_path=None,latency=0):
+Skeleton = npt.NDArray[Any] or List[int or float]
 
-        self.AKF = AKF(fs,skeleton,keypoints,model_path)
+class FLK:
+    akf: AKF
+    bca: Optional[BCA]
+    lpf: Optional[EMA]
+
+    extra_data: Any
+    
+    def __init__(self,
+                 fs : float,
+                 skeleton: Skeleton,
+                 keypoints: List[str],
+                 model_path: Optional[str]=None,
+                 latency: float=0,
+                 enable_bones: bool=True,
+                 enable_lowpass_filter: bool=True,
+                 ema_filter_value: float=0.99):
+
+        self.akf = AKF(fs,skeleton,keypoints,model_path)
                 
         # Default latency is set to 0
         self.latency = latency
         self.keypoints = keypoints
-        self.BCA = BCA(skeleton,keypoints)
+        if enable_bones:
+            self.bca = BCA(skeleton,keypoints)
         
-        self.LPF = EMA(0.99,skeleton)
+        if enable_lowpass_filter:
+            self.lpf = EMA(ema_filter_value,skeleton)
 
         # Activate LPF
         if self.latency > 0:
             pass
 
     def reset(self):
-        self.AKF.reset()
-        self.BCA.reset()
+        self.akf.reset()
+        if self.bca is not None:
+            self.bca.reset()
 
     def predict(self):
-        skeleton =  self.AKF.predict()
-        after_BCA = self.BCA.correct(skeleton,self.keypoints)
-        return after_BCA
+        skeleton =  self.akf.predict()
+        if self.bca is not None:
+            skeleton = self.bca.correct(skeleton,self.keypoints)
+        return skeleton
 
-    def correct(self,skeleton):
+    def correct(self,skeleton: Skeleton):
         
-        
-        filtered = self.AKF.correct(skeleton)
+        filtered = self.akf.correct(skeleton)
 
-        filtered = self.BCA.correct(filtered,self.keypoints)
+        if self.bca is not None:
+            filtered = self.bca.correct(filtered,self.keypoints)
 
-        filtered = self.LPF.correct(filtered)
+        if self.lpf is not None:
+            filtered = self.lpf.correct(filtered)
 
         if self.latency == 0:
-            self.AKF.old_skeleton = filtered
+            self.akf.old_skeleton = filtered
             return filtered
         
         else:
